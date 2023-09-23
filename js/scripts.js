564ff45f5383a2ac64f98ea97e0bd6b6e42fbf25
Maciel Alves Pereira <catce.2023111mtds0003@aluno.ifpi.edu.br>
2023-09-23 14:38:36 -0300

    feat: updated repository, add new layout!

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function max_screen(){
+    const janela = document.getElementById('container_main')
+    janela.classList.toggle('janela_full')
+    if (janela.classList.contains('janela_min')) {
+        janela.classList.toggle('janela_min')
+    }
+}
+
+function close_page(){
+    window.close()
+}
+
+function minimize(){
+    const janela = document.getElementById('container_main')
+    janela.classList.toggle('janela_min')
+    if (janela.classList.contains('janela_full')) {
+        janela.classList.toggle('janela_full')
+    }
+}
