7a895fd22196342d999b7cfd9c3ccce0624160e2
Maciel Alves Pereira <catce.2023111mtds0003@aluno.ifpi.edu.br>
2023-12-14 22:13:36 -0300

    feat: correção de visualização

--- js/scripts.js
+++ js/scripts.js
@@ -50,44 +50,15 @@ const mdiv = { code: 0 };
 
 mydivs.forEach(elmnt => {
     const styles = elmnt.style;
-    // styles.position = "absolute";
-    // styles.backgroundColor = "#f1f1f1";
-    // styles.textAlign = "center";
-    // styles.border = "1px solid #d3d3d3";
-    // styles.width = "200px";
-    // styles.borderRadius = "10px";
-    // styles.overflow = "hidden";
     const code = mdiv.code += 1;
     elmnt.classList.add(`item-${code}`);
     elmnt.childNodes[1].classList.add(`item-${code}-header`);
-    // console.log("Child do Mydivs->", elmnt.childNodes[1])
     dragElement(elmnt, code);
 })
 
 
 function dragElement(elmnt, code) {
     const subElmnt = document.querySelector(`.item-${code}-header`);
-    // const subElmntBtns = document.querySelector(`.item-${code}-header`).children;
-    // botão de close
-    // const btnClose = subElmnt.querySelector(".btn-close");
-    // btnClose.addEventListener("click", rmChild(elmnt));
-    // estilo de botão de close
-    // const btnCloseStyle = btnClose.style;
-    // btnCloseStyle.borderRadius = "50%";
-    // btnCloseStyle.color = "#FFF";
-    // btnCloseStyle.backgroundColor = "#555";
-    // btnCloseStyle.backgroundColor.hover = "#999";
-
-
-    // const subElmntStyles = subElmnt.style;
-    // subElmntStyles.padding = "10px";
-    // subElmntStyles.cursor = "move";
-    // subElmntStyles.zIndex = "10";
-    // subElmntStyles.backgroundColor = "#595959";
-    // subElmntStyles.color = "#fff";
-    // subElmntStyles.display = "flex";
-    // // document.body.
-    // subElmntStyles.justifyContent = "space-between";
 
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     if (subElmnt) {
@@ -116,16 +87,19 @@ function dragElement(elmnt, code) {
     function elementDrag(e) {
         e = e || window.event;
         e.preventDefault();
-        max_x = 0
-        min_x = 0
+
+        let max_x = document.body.clientWidth;
+        const min_x = 0;
+        let max_y = document.body.clientHeight;
+        const min_y = 0;
         // calculate the new cursor position:
         pos1 = pos3 - e.clientX;
         pos2 = pos4 - e.clientY;
         pos3 = e.clientX;
         pos4 = e.clientY;
         // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+        elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
     }
 
     function closeDragElement() {
